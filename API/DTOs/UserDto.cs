c9bce86fd30fb8748ffc69350148c2a5e694033f
dcrawford027 <dustin.crawford.2@gmail.com>
2023-04-11 14:55:45 -0400

    Implimented AspNetCore Identity

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+using System;
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Image { get; set; }
+        public string UserName { get; set; }
+    }
+}
